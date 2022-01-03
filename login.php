24992d4a2c33d2fd2e92af45cc20f2e390c0766f
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2022-01-03 23:48:23 +0200

    added recaptcha 2

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ include_once 'Includes/header.php';
 <h1> Log in</h1>
 <br>
 <p> To enjoy our site at full</p>
-<form action="Includes/login.inc.php" method="POST" onsubmit="return checkForm()" id="loginform">
+<form action="Includes/login.inc.php" method="POST" id="loginform" onsubmit="return validatecaptcha()">
 
 
 <div>
