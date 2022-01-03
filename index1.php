50b3f5a58bf4fc8ede69dd9460f96474e5e13bb1
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2022-01-03 22:51:01 +0200

    changed index1.php

--- index1.php
+++ index1.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="index.css"> 
     </head>
     <body> 
-    <form action="index.php" method="GET">
+    <form action="index1.php" method="GET">
         <input type="text" placeholder="Search.." name="search">
         <button type="submit">Submit</button>
 </form> 
@@ -28,15 +28,41 @@
 if(isset($_GET["search"])){
     $Name = $_GET["search"];
     if($Name !== ""){
-    $sql = "SELECT * FROM rss_info WHERE title = '$Name' ORDER BY id DESC LIMIT 20";
-    
+    $Name = "%".$Name."%";
+    $sql = "SELECT * FROM rss_info WHERE title like ?  ORDER BY id DESC LIMIT 20";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)) { 
+
+        header("Location: index1.php");
+        exit();
+
+    }
+    mysqli_stmt_bind_param( $stmt, "s", $Name);
+    mysqli_stmt_execute($stmt);
+    $result_data=mysqli_stmt_get_result($stmt);
+ 
+
+    while($row = mysqli_fetch_assoc($result_data)){
+        $title=$row["title"];
+        $linkrow=$row["link"];
+        $description=$row["description"];
+      
+        echo "
+        <item class='flex-container'>
+        <p>$title</p>
+        <p> <a href='$linkrow'> $linkrow</a></p>
+        <description>$description</description>
+       
+        </item>";
+      }
 }else {
 	
-    $sql = 'SELECT * FROM rss_info ORDER BY id DESC LIMIT 20';}}
+    $sql2 = 'SELECT * FROM rss_info ORDER BY id DESC LIMIT 20';}}
 else {
 	
-    $sql = 'SELECT * FROM rss_info ORDER BY id DESC LIMIT 20';}
-    $result = mysqli_query($conn,$sql, MYSQLI_USE_RESULT);
+    $sql2 = 'SELECT * FROM rss_info ORDER BY id DESC LIMIT 20';}
+    if(!empty($sql2)){
+    $result = mysqli_query($conn,$sql2, MYSQLI_USE_RESULT);
     while($row = $result->fetch_row()){
         $title=$row[1];
         $linkrow=$row[2];
@@ -49,7 +75,7 @@ else {
         <description>$description</description>
        
         </item>";
-      }
+      }}
 ?>
 
 </section> 
