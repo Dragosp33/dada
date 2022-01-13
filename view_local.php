e4f853bc98c1c63bfd4107e52a5e7a07a8580f72
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2022-01-13 16:00:47 +0200

    final prezentare

--- view_local.php
+++ view_local.php
@@ -99,6 +99,7 @@ require_once "Includes/connect.php";
 Include_once "Includes/header.php";
 
 if(isset($_GET["id"])){
+    $s = 0;
     $x = trim($_GET["id"]);
     $sql = "SELECT * FROM rss_info WHERE id = ?";
     if($stmt = mysqli_prepare($conn, $sql)){
@@ -111,6 +112,7 @@ if(isset($_GET["id"])){
             
             
                 while($row = mysqli_fetch_array($result_data)){
+                        $s = $row['id'];
                         echo '<div class="container">
                         <div class="title"> <a href="view_local.php?id= ' . $row['id'] . '">' . $row['title'] . '</a>
                         </div>
@@ -126,7 +128,36 @@ if(isset($_GET["id"])){
                         </div>
                         </div>';
 
-                }
+                }/*
+                $sql2 = "SELECT * FROM images WHERE rss_id = ?";      
+                if($stmt = mysqli_prepare($conn, $sql2));
+                if($stmt = mysqli_prepare($conn, $sql2)){
+                    mysqli_stmt_bind_param($stmt, "i", $param_id);
+                    $param_id = $x;
+                    if(mysqli_stmt_execute($stmt)){
+                        $result_data=mysqli_stmt_get_result($stmt);
+                     
+                    
+                        
+                        
+                            while($row = mysqli_fetch_array($result_data)){
+                                    $s = $row['id'];
+                                    echo '<div class="container">
+                                    <div class="title"> <a href="view_local.php?id= ' . $row['id'] . '">' . $row['title'] . '</a>
+                                    </div>
+                                    <div class="img" style="background-image: url(' . $row['thumbnail']. ');">
+                                    </div>
+                                    <div class="description">
+                                    <div>'. $row['oras'] . '
+                                    
+                                    </div>
+                                    <div> ' . $row['added'] . '
+                                   
+                                    </div>
+                                    </div>
+                                    </div>';
+            
+                            }*/
 
 }}
 }
