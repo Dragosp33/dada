438e147bbb5b010dfa6dddc4ab2522c9c3124b35
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2022-01-13 12:33:55 +0200

    ???????

--- newfile.php
+++ newfile.php
@@ -32,7 +32,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
         <p>Upload error :(</p>
 <?php } } ?>
         <h2>Upload a file</h2>
-        <form enctype="multipart/form-data" action="/" method="POST">
+        <form enctype="multipart/form-data" action="{{url('/') }}" method="POST">
             <input name="userfile" type="file"><input type="submit" value="Upload">
             
         </form>
